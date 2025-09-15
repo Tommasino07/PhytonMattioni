bc3aac235bc8c6666696a2d13908fe4d4d376cc7
Tommy <159021862+Tommasino07@users.noreply.github.com>
2025-09-15 09:45:52 +0000

    Punto 7 Es3

---
+++ main.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="pythonuser",
+  password="password123"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
