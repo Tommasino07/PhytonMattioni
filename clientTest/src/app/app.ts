0498db90460ec875132c75303a0a889008de021a
Tommy <159021862+Tommasino07@users.noreply.github.com>
2025-09-30 09:46:23 +0000

    Clash Royale parte iniziale

---
+++ clientTest/src/app/app.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  protected readonly title = signal('clientTest');
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getHello().subscribe({
+      next: (data: any) => console.log('Risposta dal server:', data),
+      error: (err: any) => console.error('Errore nella richiesta HTTP:', err)
+    });
+  }
+
+  getHello(): Observable<string> {
+    return this.http.get('https://stunning-space-potato-699jjrp95q7pc44jx-5002.app.github.dev/', { responseType: 'text' });
+  }
+}
