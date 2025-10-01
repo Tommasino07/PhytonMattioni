d7e0088282ba73192f80f7f8a905e494a20701b9
Tommy <159021862+Tommasino07@users.noreply.github.com>
2025-10-01 09:58:30 +0000

    Da vettore a json

--- clientTest/src/app/app.ts
+++ clientTest/src/app/app.ts
@@ -11,17 +11,18 @@ import { Observable } from 'rxjs';
 })
 export class App implements OnInit {
   protected readonly title = signal('clientTest');
+  data: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getHello().subscribe({
-      next: (data: any) => console.log('Risposta dal server:', data),
+      next: (data: any) => {console.log('Risposta dal server:', data); this.data  = data;},
       error: (err: any) => console.error('Errore nella richiesta HTTP:', err)
     });
   }
 
   getHello(): Observable<string> {
-    return this.http.get('https://stunning-space-potato-699jjrp95q7pc44jx-5002.app.github.dev/', { responseType: 'text' });
+    return this.http.get('https://stunning-space-potato-699jjrp95q7pc44jx-5000.app.github.dev/', { responseType: 'text' });
   }
 }
